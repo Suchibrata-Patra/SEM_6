1e4cc565b1b1601f1c016528f1b55b8180e5715a
PatraSuchi <trading.suchibrata.com@gmail.com>
2024-03-30 20:09:16 +0530

    Update

--- DOE/Problem Set -1.R
+++ DOE/Problem Set -1.R
@@ -67,37 +67,33 @@ M_6
 # Question No - 04
 # ===============
 
-#Question No - 04
-Y = c(19,22,20,20,29,24,30,24,26,25,16,22,28,25,31,28,27,16,27,20)
-X = c("D1","D3","D4","D1","D5","D2","D5","D3","D2","D4","D1","D2","D5","D4","D5","D4","D4","D2","D2","D3")
-data = data.frame(X,Y)
-oneway = aov(Y~as.factor(X),data=data)
+rm(list=ls())
+time = c(19,22,20,20,29,24,30,24,26,25,16,22,28,25,31,28,27,16,27,20)
+drill = c("D1","D3","D4","D1","D5","D2","D5","D3","D2","D4","D1","D2","D5","D4","D5","D4","D4","D2","D2","D3")
+data = data.frame(time,drill)
+oneway = aov(time~as.factor(drill),data=data)
 summary(oneway)
+yi0.bar=aggregate(time~as.factor(drill),data=data,mean)$time;yi0.bar
+n=aggregate(time~as.factor(drill),data=data,length)$time;n
+T=array(0);k=1;len=5
+
+for(i in 1:(len-1)){for(j in (i+1):len){
+  T[k]=(yi0.bar[i]-yi0.bar[j])/sqrt(9.04*((1/n[i])+(1/n[j])))
+  k=k+1}}
+T;qt(.975,15)
+d=ifelse(abs(T)>qt(0.975,15),"Reject","Accept")
+data.frame(T,d)
 
 
 
-                     
-t = numeric(20)
-k = 1
-for(i in 1:5){
- for(j in 1:4){
-     t[k] = (mean(y)-mean())
-
-   } 
- }
-
-
-
-
-#Question No - 05
+# =================
+# Question No - 05
+# =================
 data = c(20.6,19.5,18.1,17.9,16,19.5,19,15.6,16.7,14.1,20.5,18.5,16.3,15.2,13.7,16.2,16.5,15.7,14.8,12.7)
 length(data)
 factors = c(rep(c(25,35,50,60,70),4)) ;factors 
 letwo_way  = aov(M~factors)
 summary(two_way)
-
-
-
 for (seed_value in 1:1000) {  # Adjust the range as needed
   rm(list=ls())  # Remove all variable names before each iteration
   desired_output <- c(1, 7, 11, 17, 3, 4, 12, 15, 16, 9, 13, 14, 8, 18, 2, 5, 6, 10)
@@ -109,7 +105,3 @@ for (seed_value in 1:1000) {  # Adjust the range as needed
     break
   }
 }
-
-
-
-                                                                                                   
